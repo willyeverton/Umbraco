7fd8411e2961fe4f1e47164113b023228c291f0c
Willy Everton S. Nascimento <willy.everton.s.nascimento@gmail.com>
2020-09-22 15:58:55 -0300

    Adicionar arquivos de projeto.

---
+++ Umbraco/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using Umbraco.Web.Actions;
+using Umbraco.Models;
+using System.Net.Mail;
+
+namespace Umbraco.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+
+        public ActionResult SubmitContact(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                this.SendEmail(model);
+                return RedirectToCurrentUmbracoPage();
+            }
+            return CurrentUmbracoPage();
+        }
+
+        private void SendEmail(ContactModel model)
+        {
+            string to = "willy.everton.s.nascimento@gmail.com";
+            //string to = "hit@hitdigital.com.br";
+
+            MailMessage message = new MailMessage(model.email, to);
+            message.Subject = model.name + " - " + model.email;
+            message.Body = model.message;
+
+            SmtpClient client = new SmtpClient("172.0.0.1", 25);
+            client.Send(message);
+        }
+    }
+}
---
+++ Umbraco/Models/ContactModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Umbraco.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string name { get; set; }
+        [Required] [EmailAddress]
+        public string email { get; set; }
+        [Required]
+        public string message { get; set; }
+    }
+}
